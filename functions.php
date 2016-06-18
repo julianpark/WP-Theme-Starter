8585d3b30a755d91c1f58ab2ee7e672793095871
Julian Park <jwarnerpark@gmail.com>
2016-06-18 08:12:03 -0700

    add slug body class function

--- functions.php
+++ functions.php
@@ -3,6 +3,16 @@ register_nav_menus();
 
 add_theme_support( 'post-thumbnails' );
 
+// Page Slug Body Class
+function add_slug_body_class( $classes ) {
+  global $post;
+  if(isset($post)) {
+    $classes[] = $post->post_type . '-' . $post->post_name;
+  }
+  return $classes;
+}
+add_filter( 'body_class', 'add_slug_body_class' );
+
 /*
 add_action( 'init', 'create_post_type' );
 function create_post_type() {
