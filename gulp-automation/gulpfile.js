42ed56ef65c5b483fe83d4787a6d1f50d14335f7
Julian Park <jwarnerpark@gmail.com>
2019-12-08 22:45:44 -0800

    update gulp tasks for gulp 4

--- gulp-automation/gulpfile.js
+++ gulp-automation/gulpfile.js
@@ -3,28 +3,29 @@ var sass = require('gulp-sass');
 //var jshint = require('gulp-jshint');
 var concat = require('gulp-concat');
 
-gulp.task('sass', function() {
-  gulp.src('../sass/*.scss')
+function sassTask() {
+  return gulp.src('../sass/**/*.scss')
     .pipe(sass({ outputStyle: 'compressed' }))
-    .pipe(gulp.dest('../'));
-});
+    .pipe(gulp.dest('../')
+  );
+}
 
-gulp.task('js', function() {
-  gulp.src([
+function jsTask() {
+  return gulp.src([
     'node_modules/jquery/dist/jquery.min.js',
     'node_modules/bootstrap/dist/js/bootstrap.min.js',
     '../js/vendors/*.js',
     '../js/modules/*.js'
     ])
-    //.pipe(jshint())
-    //.pipe(jshint.reporter('fail'))
     .pipe(concat('main.js'))
     .pipe(gulp.dest('../js/'));
-});
+}
 
-gulp.task('watch', function() {
-  gulp.watch('../sass/**/*.scss', gulp.parallel('sass'));
-  gulp.watch('../js/**/*.js', gulp.parallel('js'));
-});
+function watchTask() {
+  gulp.watch('../sass/**/*.scss', sassTask);
+  gulp.watch('../js/**/*.js', jsTask);
+}
 
-gulp.task('default', gulp.parallel('sass', 'js', 'watch'));
+exports.default = gulp.series(
+  gulp.parallel(sassTask, jsTask),
+  watchTask);
