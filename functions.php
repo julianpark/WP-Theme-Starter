014a88e5a2887682ad0e9874dc8afdab6656cf53
Julian Park <jwarnerpark@gmail.com>
2022-03-10 16:27:06 -0800

    add new nl functions

--- functions.php
+++ functions.php
@@ -30,3 +30,35 @@ function create_post_type() {
     )
   );
 }*/
+
+/* ACF Options Page
+if( function_exists('acf_add_options_page') ) {
+	acf_add_options_page();
+}*/
+
+function nl2span($copy) {
+  // wrap new lines in desktop line breaking span
+  $copy = preg_replace('/(<[^>]+) style=".*?"/i', '$1', $copy); // remove style attribute
+  $copy = wpautop($copy);
+  $copy = str_replace('<p>', '<span class="d-lg-block">', $copy);
+  $copy = str_replace('</p>', '</span>', $copy);
+  return $copy;
+}
+
+function nl2br_strip_p($copy) {
+  // wrap new lines in desktop line breaking span
+  $copy = nl2br(trim($copy));
+  $copy = str_replace('<p>', '', $copy);
+  $copy = str_replace('</p>', '', $copy);
+  return $copy;
+}
+
+function nl2li($copy) {
+  // wrap new lines in desktop line breaking span
+  $copy_trim = trim($copy);
+  $str = explode("\r\n", $copy_trim);
+  foreach($str as $key => $value) {
+    $copy_new .= '<li>' . $value . '</li>';
+  }
+  return $copy_new;
+}
